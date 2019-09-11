054aef222a130d97fb9b90db97ed45e6f3ec41da
chizzy <cwchiz16@gmail.com>
2019-09-11 11:11:12 -0600

    add crystal values and show total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var wins = 0;
+    var losses = 0;
     // global number
     var maxGlobal = 120;
     var minGlobal = 19;
@@ -34,19 +36,56 @@ $(document).ready(function() {
 
       console.log(redCrystal, blueCrystal, yellowCrystal, greenCrystal);
 
+      $("#totalScore").attr("data-sum", 0);
+      
+
     // button clicks
     $("#redCrystal").click(function() {
-        $("#totalScore").html(redCrystal);
+        // $("#totalScore").html(redCrystal);
+
+        var newSum = $("#totalScore").data("sum");
+        newSum += redCrystal;
+        $("#totalScore").data("sum", newSum);
+        $("#totalScore").html(newSum);
+
+        console.log("red crystal: ", redCrystal);
+        console.log("total score: ", newSum);
     });
     $("#blueCrystal").click(function() {
-        $("#totalScore").html(blueCrystal);
+        // $("#totalScore").html(blueCrystal);
+        var newSum = $("#totalScore").data("sum");
+        newSum += blueCrystal;
+        $("#totalScore").data("sum", newSum);
+        $("#totalScore").html(newSum);
+
+        console.log("blue crystal: ", blueCrystal);
+        console.log("total score: ", newSum);
     });
     $("#yellowCrystal").click(function() {
-        $("#totalScore").html(yellowCrystal);
+        // $("#totalScore").html(yellowCrystal);
+        var newSum = $("#totalScore").data("sum");
+        newSum += yellowCrystal;
+        $("#totalScore").data("sum", newSum);
+        $("#totalScore").html(newSum);
+
+        console.log("yellow crystal: ", yellowCrystal);
+        console.log("total score: ", newSum);
     });
     $("#greenCrystal").click(function() {
-        $("#totalScore").html(greenCrystal);
+        // $("#totalScore").html(greenCrystal);
+        var newSum = $("#totalScore").data("sum");
+        newSum += greenCrystal;
+        $("#totalScore").data("sum", newSum);
+        $("#totalScore").html(newSum);
+
+        console.log("greenCrystal: ", greenCrystal);
+        console.log("total score: ", newSum);
     });
-    var totalScore = []
+    
+    if(newSum === globalNumber) {
+        wins++
+        console.log(wins)
+    }
+
     });
     
